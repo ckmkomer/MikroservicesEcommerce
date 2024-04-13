40ff3c18332326982117b486c37560b443958bc1
Ömer Çakmak <ckmkomr1@gmail.com>
2024-04-14 00:26:17 +0300

    ProductDetail

---
+++ Services/Catalog/MikroservicesEcommerce.Catalog/Services/ProductDetailServices/IProductDetailService.cs
@@ -0,0 +1,15 @@
+﻿using MikroservicesEcommerce.Catalog.Dtos.CategoryDtos;
+using MikroservicesEcommerce.Catalog.Dtos.ProductDetailDtos;
+using MikroservicesEcommerce.Catalog.Dtos.ProductDtos;
+
+namespace MikroservicesEcommerce.Catalog.Services.ProductDetailServices
+{
+    public interface IProductDetailService
+    {
+        Task<List<ResultProductDetailDto>> GetAllProductAsync();
+        Task CreateProductDetailAsync(CreateProductDetailDto createProductDetailDto);
+        Task UpdateProductDetailAsync(UpdateProductDetailDto updateProductDetailDto);
+        Task DeleteProductDetailAsync(String id);
+        Task<GetByIdProductDetailDto> GetByIdProductDetailAsync(String id);
+    }
+}
---
+++ Services/Catalog/MikroservicesEcommerce.Catalog/Services/ProductDetailServices/ProductDetailService.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using MikroservicesEcommerce.Catalog.Dtos.ProductDetailDtos;
+using MikroservicesEcommerce.Catalog.Entities;
+using MikroservicesEcommerce.Catalog.Settings;
+using MongoDB.Driver;
+
+namespace MikroservicesEcommerce.Catalog.Services.ProductDetailServices
+{
+    public class ProductDetailService : IProductDetailService
+    {
+
+        private readonly IMongoCollection<ProductDetail> _productDetailCollection;
+        private readonly IMapper _mapper;
+
+        public ProductDetailService(IMapper mapper ,IDatabaseSettings _databaseSettings)
+        {
+            var client = new MongoClient(_databaseSettings.ConnectionString);
+            var databese = client.GetDatabase(_databaseSettings.ProductDetailCollectionName);
+            _productDetailCollection = databese.GetCollection<ProductDetail>(_databaseSettings.ProductDetailCollectionName);
+            _mapper = mapper;
+        }
+
+        public async Task CreateProductDetailAsync(CreateProductDetailDto createProductDetailDto)
+        {
+            var values = _mapper.Map<ProductDetail>(createProductDetailDto);
+            await _productDetailCollection.InsertOneAsync(values);
+        }
+
+        public async Task DeleteProductDetailAsync(string id)
+        {
+            await _productDetailCollection.DeleteOneAsync(id);
+        }
+
+        public async Task<List<ResultProductDetailDto>> GetAllProductAsync()
+        {
+            var values = await _productDetailCollection.Find(c)
+        }
+
+        public Task<GetByIdProductDetailDto> GetByIdProductDetailAsync(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateProductDetailAsync(UpdateProductDetailDto updateProductDetailDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
